5beb2e90458fe85d47567f0cfa8cacd32134ea4a
Vishnu4486 <biradarvishnu89@gmail.com>
2020-07-07 10:27:30 +0530

    first commit

---
+++ PPMToolFullStack/src/main/java/com/yash/pms/PpmToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.yash.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+
+}
---
+++ PPMToolFullStack/src/test/java/com/yash/pms/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
